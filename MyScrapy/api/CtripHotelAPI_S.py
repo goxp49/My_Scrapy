94c799d1846223d2c86b28d01396b8f276451b22
goxp49 <215920363@qq.com>
2018-07-18 21:39:16 +0800

    正在重写携程网爬取流程，发现有蜜罐存在，正在解析。

--- MyScrapy/api/CtripHotelAPI_S.py
+++ MyScrapy/api/CtripHotelAPI_S.py
@@ -31,6 +31,17 @@ index_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
     'Host': 'hotels.ctrip.com',
 }
+
+list_headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    'Host': 'hotels.ctrip.com',
+    'Origin': 'http://www.ctrip.com',
+    'Referer': 'http://www.ctrip.com/',
+}
+
 # 通过selenium的方式获取酒店价格（速度慢）
 def GetCtripHotelIformation(urls):
     # 获取配置参数，可进行修改
@@ -135,9 +146,9 @@ def SearchCtripHotelUrl(keywork):
     return url_list
 '''
 
+
 # 通过API获取指定对应城市的索引
 def GetCityIndex(city):
-
     url = 'http://hotels.ctrip.com/Domestic/Tool/AjaxDestination.aspx'
     # 设置请求内容
     data = {
@@ -159,6 +170,41 @@ def GetCityIndex(city):
     # 返回结果,检查结果是否包含城市名称,不包含则返回None
     return process_str[0] + process_str[2] if city in process_str[1] else None
 
+
+# 通过城市索引获取酒店搜索结果
+def GetHotelList(city_index, city):
+    new_url = 'http://hotels.ctrip.com/hotel/shanghai2/k1%E5%A6%82%E5%AE%B6#ctm_ref=hod_hp_sb_lst'
+    # url = 'http://hotels.ctrip.com/hotel/%s/k1%s' % (city_index, city)
+    # 转换含中文的url地址
+    #new_url = urllib.parse.quote(url)
+    # 设置请求内容
+    data = {
+        'CityName':'%C9%CF%BA%A3',
+        'cityId': 2,
+        'cityPY': 'Shanghai',
+        'checkIn': '2018-7-18',
+        'checkOut': '2018-07-19',
+        'RoomGuestCount': '1,1,0',
+        'keywordNew': '%C8%E7%BC%D2',
+        'keyword': '%C8%E7%BC%D2',
+        'Cookie': '_abtest_userid=61af2760-de3d-4762-8768-59278b49706c; _ga=GA1.2.2071221705.1530883501; MKT_Pagesource=PC; _RSG=KCalN5l8Na708LIPiN61K9; _RDG=2816579724a37b2931057df9cc00ae9c27; _RGUID=791722f1-57be-4314-9501-197b19495200; StartCity_Pkg=PkgStartCity=258; Corp_ResLang=zh-cn; _RF1=124.72.244.136; _gid=GA1.2.999617163.1531829417; ASP.NET_SessionId=n1rrx0cdudpitcckj0uuszd4; OID_ForOnlineHotel=153088349671813phf21531916749911102002; appFloatCnt=11; manualclose=1; Session=smartlinkcode=U130026&smartlinklanguage=zh&SmartLinkKeyWord=&SmartLinkQuary=&SmartLinkHost=; Union=AllianceID=4897&SID=130026&OUID=&Expires=1532521761664; HotelDomesticVisitedHotels1=1569729=0,0,4.5,9200,/200w080000002zosp5BBE.jpg,&436187=0,0,4.4,8705,/200n0s000000hhavcFF0F.jpg,&472757=0,0,4.4,1779,/fd/hotel/g3/M09/72/2B/CggYGVY8GlmAJ_RjABA9tOa05Eg752.jpg,&981622=0,0,4.5,808,/200u0q000000g4c2o517C.jpg,&978086=0,0,4.5,798,/hotel/439000/438574/2f85a12c2f044858971a3337d73aef50.jpg,&975443=0,0,4.5,3084,/200a0r000000hcwpm2AEA.jpg,; HotelCityID=2split%E4%B8%8A%E6%B5%B7splitShanghaisplit2018-7-18split2018-07-19split0; _gat=1; _bfa=1.1530883496718.13phf2.1.1531829411686.1531916698006.11.170.600001375; _bfs=1.36; Mkt_UnionRecord=%5B%7B%22aid%22%3A%224897%22%2C%22timestamp%22%3A1531920353093%7D%5D; __zpspc=9.12.1531916961.1531920353.28%232%7Cwww.baidu.com%7C%7C%7C%25E6%2590%25BA%25E7%25A8%258B%7C%23; _jzqco=%7C%7C%7C%7C%7C1.1003814229.1530883501572.1531920317303.1531920353131.1531920317303.1531920353131.0.0.0.142.142; _bfi=p1%3D100101991%26p2%3D102002%26v1%3D170%26v2%3D168',
+    }
+
+    data_bytes = bytes(urllib.parse.urlencode(data), encoding='utf8')
+    #print(new_url)
+    request = urllib.request.Request(url=new_url, data=data_bytes, headers=list_headers)
+    response = urllib.request.urlopen(request)
+    print(response.info())
+    print(response.getcode())
+    print(response.geturl())
+    content = response.read()  # content是压缩过的数据
+    buff = BytesIO(content)  # 把content转为文件对象
+    f = gzip.GzipFile(fileobj=buff)
+    # 获得原始结果'cQuery.jsonpResponse={"key":"北京","data":"@Beijing|北京|1|15660|北京||Cit………………'
+    print(f.read().decode('utf-8'))
+
+
+
 # 获取对应酒店最低价格
 # 流程：获取城市对应索引 --> 获取目标酒店ID --> 获取目标酒店价格
 def GetCtripHotelLowestPrice(city, keyword, start_time, end_time):
@@ -168,5 +214,6 @@ def GetCtripHotelLowestPrice(city, keyword, start_time, end_time):
 if __name__ == '__main__':
     # SearchHotelUrl('璞宿酒店')
     # GetCtripHotelIformation(['http://hotels.ctrip.com/hotel/3680675.html', 'http://hotels.ctrip.com/hotel/8020262.html'])
-    #GetCtripHotelIformation(SearchCtripHotelUrl('璞宿酒店'))
-    print(GetCityIndex('尼玛'))
+    # GetCtripHotelIformation(SearchCtripHotelUrl('璞宿酒店'))
+    #print(GetCityIndex('尼玛'))
+    GetHotelList('1','2')

c91031c624e489fb5b4202e32fb6165056bd551e
goxp49 <215920363@qq.com>
2018-07-24 21:25:26 +0800

    已正常获取价格，准备整合。

--- MyScrapy/api/QunarHotelAPI.py
+++ MyScrapy/api/QunarHotelAPI.py
@@ -5,7 +5,6 @@ import operator  # 导入运算符模块
 from pandas.tseries.offsets import Day
 from bs4 import BeautifulSoup
 
-
 bed_list = ['', '大床', '双床', '', '多床']
 network_list = ['没有网络', '提供WIFI', '有线网络']
 
@@ -28,28 +27,27 @@ list_headers = {
 }
 
 
-
 def GetCityIndex(city):
     # 设定去哪儿城市index API地址
     api_url = 'http://hs.qunar.com/api/hs/citysugV3'
-    #设置请求内容
+    # 设置请求内容
     data = {
         'city': city,
     }
     data_bytes = urllib.parse.urlencode(data)
-    new_url = api_url + "?" + data_bytes  #URL拼接
+    new_url = api_url + "?" + data_bytes  # URL拼接
     # print(new_url)
     request = urllib.request.Request(url=new_url, headers=index_headers)
     response = urllib.request.urlopen(request)
     content = response.read()  # content是压缩过的数据
     buff = BytesIO(content)  # 把content转为文件对象
     f = gzip.GzipFile(fileobj=buff)
-    #print(json.loads(f.read().decode('utf-8'))['data'])
+    # print(json.loads(f.read().decode('utf-8'))['data'])
     try:
         result_json = json.loads(f.read().decode('utf-8'))['data']
         if result_json:
             for result in result_json:
-                #由于JSON已经在服务器排序过，所以第一个结果是最优搜索
+                # 由于JSON已经在服务器排序过，所以第一个结果是最优搜索
                 if city in result['c']:
                     return result['o']
         return False
@@ -57,15 +55,15 @@ def GetCityIndex(city):
         return False
 
 
-def GetTargetHotelList(keywords):
+def GetTargetHotelList(city, city_index, keywords, start_time, end_time):
     api_url = 'https://touch.qunar.com/hotel/hotellist'
     # 设置请求内容
     data = {
-        'city': '上海',
-        'cityUrl': 'shanghai_city',
-        'checkInDate': '2018-07-24',
-        'checkOutDate': '2018-07-30',
-        'keywords': '丽思卡尔顿',
+        'city': city,
+        'cityUrl': city_index,
+        'checkInDate': start_time,
+        'checkOutDate': end_time,
+        'keywords': keywords,
         'extra': '{"L":"","DU":"","MIN":0,"MAX":0}',
     }
     data_bytes = urllib.parse.urlencode(data)
@@ -79,8 +77,8 @@ def GetTargetHotelList(keywords):
     html = f.read().decode('utf-8')
     soup = BeautifulSoup(html, 'lxml')
     hotel_soups = soup.find_all('li', 'qt-bb-x1')
-    price_min = 99999   # 初始化最低价格
-    lowest_hotel_url = None
+    price_min = 99999  # 初始化最低价格
+    lowest_hotel_id = None
     for hotel in hotel_soups:
         hotel_name = hotel.find('p', 'hotel-title').string
         hotel_available = hotel.find('span', 'qt-mr5')
@@ -89,11 +87,46 @@ def GetTargetHotelList(keywords):
         print(hotel_price)
         if keywords in hotel_name and int(hotel_price) <= price_min:
             price_min = int(hotel_price)
-            lowest_hotel_url = hotel.select_one('div.list-info a').get('href')
-    return lowest_hotel_url
+            lowest_hotel_id = hotel.get('data-id')
+    return lowest_hotel_id
+
+
+def GetTargetHotelPrice(hotel_id, hotel_name,start_time, end_time):
+    api_url = 'https://touch.qunar.com/api/hotel/hotelprice'
+    # 设置请求内容
+    data = {
+        'seq': 'shanghai_city_24925',
+        'checkInDate': '2018-07-24',
+        'checkOutDate': '2018-07-25',
+        'type': 0,
+    }
+    data_bytes = urllib.parse.urlencode(data)
+    new_url = api_url + "?" + data_bytes  # URL拼接
+    # print(new_url)
+    request = urllib.request.Request(url=new_url, headers=list_headers)
+    response = urllib.request.urlopen(request)
+    content = response.read()  # content是压缩过的数据
+    buff = BytesIO(content)  # 把content转为文件对象
+    f = gzip.GzipFile(fileobj=buff)
+    roomlist = json.loads(f.read().decode('utf-8'))['data']['rooms']
+    price_min = 99999
+    result_dict = {}
+    print(roomlist)
+    for room in roomlist:
+        bed_type = room['name']
+        price = int(room['lowPrice'])
+        if price <= price_min:
+            price_min = price
+            result_dict['name'] = hotel_name
+            result_dict['bed_type'] = bed_type
+            result_dict['price'] = price
+            result_dict['url'] = hotel_id.split('_')[-1:]
+            print(result_dict['url'])
+        print(bed_type)
+        print(price)
+
 
 if __name__ == '__main__':
-    start_time = datetime.datetime.now().strftime('%Y-%m-%d')
-    end_time = (datetime.datetime.now() + Day()).strftime('%Y-%m-%d')
-    #GetCtripHotelUrl('锦江都城酒店')  # 富豪/随意/丽思卡尔顿
-    print(GetTargetHotelList('丽思卡尔顿'))
+    # GetCtripHotelUrl('锦江都城酒店')  # 富豪/随意/丽思卡尔顿
+    # print(GetTargetHotelList('上海', 'shanghai_city', '丽思卡尔顿', '2018-07-24', '2018-07-26'))
+    print(GetTargetHotelPrice('shanghai_city_24925','n',1,1))
